dfa156e2d8b1faefc698c73441f38a45155fa390
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-29 23:16:22 +0100

    modified my user model to store password and apikey of user generated during registration

--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,17 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // to store APIkey generated during registration
+  apikey: {
+    type: String,
+    required: true,
+    unique: true,
   },
   // to track the confirmation status of a user
   confirmed: {
