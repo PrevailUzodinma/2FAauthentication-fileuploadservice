44df2b8e5cda8b67e52428faf3b2b9bc85915b0b
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-30 00:18:47 +0100

    added router to app.js, time to test the register route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/mongodb");
+const router = require("./routes/index.router");
 const app = express();
 
 // Connect to Database
@@ -16,6 +17,9 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+// use router
+app.use('/api/v1/', router)
 
 // start the server and listen
 const PORT = process.env.PORT || 3000;
