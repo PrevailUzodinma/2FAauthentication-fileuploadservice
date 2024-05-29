6b12c5b2046e66384c3dbb4bd7e50864e5c59025
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-29 21:06:22 +0100

    set up app.js(server)

---
+++ app.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// allow requests from any origin
+app.use(cors({}));
+
+// logger middleware: log endpoints called
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// start the server and listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
