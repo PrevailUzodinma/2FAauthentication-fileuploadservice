ff724bbd54257051b654f1106887f57245b6e2fc
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-29 21:10:24 +0100

    require config/mongodb.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/mongodb");
 const app = express();
 
 // Connect to Database
