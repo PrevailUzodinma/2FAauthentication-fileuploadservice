2347f2fa069df84829bab76fc4c1a2af9fd95647
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-31 00:37:47 +0100

    setting up redis, generating otp and trying to use it in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const UserService = require("../services/user.service");
+const redisClient = require("../config/redis.config")
 const sendEmail = require("../utils/email");
+const generateOtp = require("../services/otp.service")
 
 class UserController {
   async register(req, res) {
@@ -34,31 +36,72 @@ class UserController {
     try {
       // Retrieve id from request parameters
       const { id } = req.params;
-  
+
       // Find user by id in the database
       const existingUser = await UserService.findUserById(id);
-  
+
       if (!existingUser) {
         return res.status(404).json({
           success: false,
           message: "Oops! Not Found. User does not exist",
         });
       }
-  
+
       // Confirm user
       existingUser.confirmed = true;
       await existingUser.save();
-  
+
       // Send success response
       res.status(200).json({
         success: true,
-        message: "Congratulations! Your email has been confirmed, you may proceed to login",
+        message:
+          "Congratulations! Your email has been confirmed, you may proceed to login",
       });
     } catch (error) {
       // Handle errors
       res.status(500).send(error.message);
     }
-  }  
+  }
+
+  async login(req, res) {
+    try {
+      // retrieve email and password from request body
+      const { email, password } = req.body;
+
+      // check if user exists and if email is confirmed
+      const existingUser = await UserService.findUserByEmail(email);
+
+      if (!existingUser) {
+        return res.status(404).json({ message: "User does not exist" });
+      } else if (existingUser.confirmed === false) {
+        return res
+          .status(400)
+          .json({
+            message:
+              "User email has not been confirmed, please confirm your email before logging in",
+          });
+      }
+      // take user password and compare to entered password
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid Password" });
+      }
+
+      // Generate OTP and save it in Redis with a 5-minute expiration
+      const otp = generateOtp();
+      await redisClient.setex(`otp:${existingUser._id}`, 300, otp); // 300 seconds = 5 minutes
+
+       // Send OTP to user's email
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error occurred while login", error: error });
+    }
+  }
 
   async fetchAll(req, res) {
     try {
---
+++ services/otp.service.js
@@ -0,0 +1,8 @@
+const generateOTP = () => {
+    // Generate a 6-digit OTP
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  };
+
+  module.exports = {
+generateOTP
+  };
