3685544c5c50e303467fdf675358aff6d22e0136
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-06-01 00:42:18 +0100

    added the validateapikey middleware to the file upload route

--- routes/file.router.js
+++ routes/file.router.js
@@ -1,9 +1,10 @@
 const router = require ("express").Router();
 const multer = require('multer');
+const validateApiKey = require("../middlewares/validateApikey.middleware")
 const FileController = require('../controllers/file.controller');
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/upload', upload.single('file'), FileController.uploadFile);
+router.post('/upload', upload.single('file'), validateApiKey, FileController.uploadFile);
 
 module.exports = router;
