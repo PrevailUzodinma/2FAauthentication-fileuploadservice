52351b13f5214b9032503b98de81b36fe4c70552
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-06-01 00:14:30 +0100

    created file service for allowed file extension and to save file to a user

---
+++ services/file.service.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const User = require("../models/user.model");
+
+const UPLOAD_FOLDER = "./uploads/";
+const ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"];
+
+class FileService {
+    
+  allowedFile(file) {
+    const ext = path.extname(file.originalname).toLowerCase();
+    return ALLOWED_EXTENSIONS.includes(ext.substring(1));
+  }
+
+  async saveFileToUser(user, file) {
+    const imgData = fs.readFileSync(file.path);
+    const base64Data = imgData.toString("base64");
+    const contentType = file.mimetype;
+
+    user.images.push({
+      filename: file.originalname,
+      data: base64Data,
+      contentType: contentType,
+    });
+
+    await user.save();
+
+    // Delete file from filesystem
+    fs.unlinkSync(file.path);
+  }
+}
+
+module.exports = { allowedFile, saveFileToUser };
