35bf5bd0c2f02d2a2a125f8d9c60f22b5437eb46
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-05-31 03:32:44 +0100

    set up my verify otp route

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,9 +1,11 @@
 const router = require ("express").Router();
 const UserController = require("../controllers/user.controller");
+const verifyToken = require("../middlewares/verifyToken.middleware");
 
 router.post('/register', UserController.register);
 router.get('/confirm/:id', UserController.confirm);
 router.post('/login', UserController.login)
+router.post('/verify-otp', verifyToken, UserController.verifyOtp)
 router.get("/", UserController.fetchAll);
 router.delete('/:id', UserController.deleteUser);
 
