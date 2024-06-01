13ba094eaf8afc8e898808d49703e2ddab9cbd24
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-06-01 16:37:52 +0100

    set up route to get all files and a single file(image)

--- routes/file.router.js
+++ routes/file.router.js
@@ -4,6 +4,13 @@ const verifyApikey = require("../middlewares/verifyApikey.middleware")
 const FileController = require('../controllers/file.controller');
 const upload = require('../config/multer.config'); // Import the Multer configuration
 
+// Route to upload file
 router.post('/upload', verifyApikey, upload.single('file'), FileController.uploadFile);
+
+// Route to get all files
+router.get("/", FileController.getAllFiles);
+
+// Route to get a single file by its id
+router.get("/:id", FileController.getFileById);
 
 module.exports = router;
